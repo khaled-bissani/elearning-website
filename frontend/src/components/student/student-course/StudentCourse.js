fd708b25c4f8c840fc1406c76f64605764fdac04
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-09 12:54:42 +0300

    Frontend <> Student page: adding sidebar and content component in a parent component

---
+++ frontend/src/components/student/student-course/StudentCourse.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SideBar from '../SideBar';
+import Content from './Content';
+
+const StudentCourse = () => {
+  return (
+    <div className='page-form'>
+        <SideBar />
+        <Content />
+    </div>
+  )
+}
+
+export default StudentCourse
