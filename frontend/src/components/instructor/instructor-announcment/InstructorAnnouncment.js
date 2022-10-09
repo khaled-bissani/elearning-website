d9780e20a61509a6690fe8cb2ecd5ea088c14f19
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-09 12:26:47 +0300

    Frontend <> Instructor page: adding sidebar and content component in a parent component

---
+++ frontend/src/components/instructor/instructor-announcment/InstructorAnnouncment.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SideBar from '../SideBar';
+import Content from './Content';
+
+const InstructorAnnouncment = () => {
+  return (
+    <div className='page-form'>
+        <SideBar />
+        <Content />
+    </div>
+  )
+}
+
+export default InstructorAnnouncment
