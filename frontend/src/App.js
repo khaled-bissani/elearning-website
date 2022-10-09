ba5ea36833ef80f910fa9ab3148018e5f94a25f5
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-09 13:06:33 +0300

    Frontend <> App file: adding the login page component when the website runs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import LoginPage from './components/login/LoginPage';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <LoginPage/>
     </div>
   );
 }
