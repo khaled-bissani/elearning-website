384703034cfb534b76dafaf6ce0740ed4955aca2
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-11 12:01:55 +0300

    Backend <> User: adding all the relations with other tables

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -7,5 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-    use HasFactory;    
+    use HasFactory; 
+    
+    public function courses()
+    {
+        return $this->belongsToMany(Course::class, 'assignments', 'users_id', 'courses_id')
+        ->belongsToMany(Course::class, 'announcments', 'users_id', 'courses_id')
+        ->belongsToMany(Course::class, 'assigns', 'users_id', 'courses_id')
+        ->belongsToMany(Course::class, 'enrolls', 'users_id', 'courses_id');
+    }
 }
